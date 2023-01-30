042e670640e49cbeff52c3ee9ca29b9f459b75a2
Navigoo <81353827+Navigoo@users.noreply.github.com>
2023-01-30 11:20:23 +0100

    3

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 /* Welcome to VUE Lesson week 5 */
 /* simple log output */
-console.log('Hej nu körs jag via node server')
+/* console.log('Hej nu körs jag via node server')
 
 /* Function */
-
-const test = (name) => {
+/* const test = (name) => {
     return `Hej nu körs jag via en funktion: ${name}`
 }
 
-console.log(test('bertil'))
+console.log(test('bertil')) */
 
+/* Install package  with npm
+/*  */
+
+ 
